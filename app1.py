40c00e06f9cecf9898f1319752200141396a5172
Carlos <adrian.ag666@gmail.com>
2021-03-04 00:36:23 -0600

    validado por si la palabra no esta en el dic

--- app1.py
+++ app1.py
@@ -6,7 +6,7 @@ def translate (w):
     if w in data:
         return data[w]
     else:
-        return "The word doesn´t exist. Please double check it."
+        return "The word doesn´t exist.Please double check it."
 
 word = input("Enter word: ")
 
