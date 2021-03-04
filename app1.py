370f09a68a79e528b2bbf71753c8b5cfd36b6a53
Carlos <adrian.ag666@gmail.com>
2021-03-04 01:53:20 -0600

    Finalizado validando NombresPropios

--- app1.py
+++ app1.py
@@ -5,8 +5,14 @@ data = json.load(open("data.json"))
 
 def translate (w):
     w = w.lower()
+    
     if w in data:
         return data[w]
+    
+    elif w.capitalize() in data:
+        return data[w.capitalize()]
+    
+    
     elif len(get_close_matches(w, data.keys()))>0:
         yn =input( "Did you mean %s instead? Enter Y if yes or N if no." % get_close_matches(w, data.keys())[0])
         if yn == "Y":
